ded86f146c5e7915225e123e5e3fa19966120438
00gasman00 <00gasman00@gmail.com>
2026-06-29 15:29:49 -0400

    Fix homepage redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import {Redirect} from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/getting-started/what-is-gas-preprotools" />;
+  return <Redirect to="/gas-preprotools-docs/getting-started/what-is-gas-preprotools" />;
 }
